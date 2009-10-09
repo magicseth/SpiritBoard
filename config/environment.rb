fbed947e73fe1b0d2e60629d7c996c01d9ce23ce
Seth Raphael <github@magicseth.com>
2009-10-09 10:48:31 -0700

    adding gem path

--- config/environment.rb
+++ config/environment.rb
@@ -1,3 +1,5 @@
+ENV['GEM_PATH'] = '/home/betterma/gems:/usr/lib/ruby/gems/1.8'
+
 # Be sure to restart your server when you modify this file
 
 # Specifies gem version of Rails to use when vendor/rails is not present
